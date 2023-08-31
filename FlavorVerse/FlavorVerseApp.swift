6dbe36d2135bc687d214053cf384a02c3dbb2bff
Lunick Francois <LunickFrancois22@gmail.com>
2023-08-31 13:59:28 -0400

    Initial Commit

---
+++ FlavorVerse/FlavorVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorVerseApp.swift
+//  FlavorVerse
+//
+//  Created by Lunick Francois on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct FlavorVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
