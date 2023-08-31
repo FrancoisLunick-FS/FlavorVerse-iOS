ff31518ae7c8b256587243474c70ced09249bedc
Lunick Francois <LunickFrancois22@gmail.com>
2023-08-31 16:55:36 -0400

    Create ContentViewModel.swift

---
+++ FlavorVerse/ContentViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ContentViewModel.swift
+//  FlavorVerse
+//
+//  Created by Lunick Francois on 8/31/23.
+//
+
+import Foundation
+import Firebase
+import Combine
+
+class ContentViewModel: ObservableObject {
+    
+    private let service = AuthService.shared
+    private var cancellables = Set<AnyCancellable>()
+    
+    @Published var userSession: FirebaseAuth.User?
+    
+    init() {
+        
+    }
+    
+    func setup() {
+        service.$userSession.sink { [weak self] userSession in
+            self?.userSession = userSession
+        }
+        .store(in: &cancellables)
+    }
+}
