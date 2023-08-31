6aca5d8568ef58ce89854a49ab4a8f031df9c817
Lunick Francois <LunickFrancois22@gmail.com>
2023-08-31 16:11:58 -0400

    Add Functions to AuthService
    Add functions that handle the login, creation, and sign out of the user

--- FlavorVerse/Authentication/Service/AuthService.swift
+++ FlavorVerse/Authentication/Service/AuthService.swift
@@ -9,4 +9,26 @@ import Foundation
 
 class AuthService {
     
+    /// Handles user login.
+    /// - Parameters:
+    ///   - email: user's email
+    ///   - password: user's password
+    func login(withemail email: String, password: String) async throws {
+        
+    }
+    
+    /// Handles the creaton of a new user account.
+    /// - Parameters:
+    ///   - email: new user's email
+    ///   - password: new user's password
+    ///   - username: new user's username
+    func createUser(usingEmail email: String, password: String, username: String) async throws {
+        
+    }
+    
+    
+    /// Signs the user out
+    func signOut() {
+        
+    }
 }
