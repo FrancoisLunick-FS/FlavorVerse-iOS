da6ad17613efdd07a0b332e0fbe77c4416f79099
Lunick Francois <LunickFrancois22@gmail.com>
2023-08-31 21:19:40 -0400

    Create RecipeCardView.swift

---
+++ FlavorVerse/View/RecipeCardView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeCardView.swift
+//  FlavorVerse
+//
+//  Created by Lunick Francois on 8/31/23.
+//
+
+import SwiftUI
+
+struct RecipeCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeCardView()
+    }
+}
