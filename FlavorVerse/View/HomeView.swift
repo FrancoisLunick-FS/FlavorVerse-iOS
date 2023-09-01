f088565b3ef34630164b40efc6f2fb11d3e603a4
Lunick Francois <LunickFrancois22@gmail.com>
2023-08-31 21:19:45 -0400

    Create HomeView.swift

---
+++ FlavorVerse/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  FlavorVerse
+//
+//  Created by Lunick Francois on 8/31/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
