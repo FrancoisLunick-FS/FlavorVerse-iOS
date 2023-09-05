88d05102700946145aba411b29a23ac3105bf46c
Lunick Francois <LunickFrancois22@gmail.com>
2023-09-04 20:10:39 -0400

    Update RecipeModel.swift
    Add test data for recipe

--- FlavorVerse/RecipeModel.swift
+++ FlavorVerse/RecipeModel.swift
@@ -20,8 +20,26 @@ class RecipeModel: Identifiable {
     let description: String
     let ingredients: String
     let directions: String
-    let category: Category
+    let category: Category.RawValue
     let datePublished: String
     let url: String
     
+    init(name: String, image: String, description: String, ingredients: String, directions: String, category: Category.RawValue, datePublished: String, url: String) {
+        self.name = name
+        self.image = image
+        self.description = description
+        self.ingredients = ingredients
+        self.directions = directions
+        self.category = category
+        self.datePublished = datePublished
+        self.url = url
+    }
+    
+}
+
+extension RecipeModel {
+    static let recipes: [RecipeModel] =
+    [
+        RecipeModel(name: "Test", image: "test.jpg", description: "this is a test", ingredients: "testing", directions: "test", category: "Lunch", datePublished: "9/9/2023", url: "test.com")
+    ]
 }
