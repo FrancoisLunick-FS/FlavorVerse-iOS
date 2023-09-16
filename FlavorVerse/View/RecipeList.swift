886c5b2112fdce16b3dd134c6019caeaa263e4eb
Lunick Francois <LunickFrancois22@gmail.com>
2023-09-15 21:44:02 -0400

    Update RecipeList.swift

--- FlavorVerse/View/RecipeList.swift
+++ FlavorVerse/View/RecipeList.swift
@@ -9,10 +9,17 @@ import SwiftUI
 
 struct RecipeList: View {
     var recipes: [RecipeModel]
+    var category: Category
     var body: some View {
         
         VStack {
-            Text("Your Recipes are below")
+            HStack {
+                Text(category.rawValue)
+                    .font(.title.bold())
+                    .padding(.vertical)
+                
+                Spacer()
+            }
             
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 160), spacing: 15)], spacing: 15) {
                 
@@ -29,7 +36,7 @@ struct RecipeList: View {
 struct RecipeList_Previews: PreviewProvider {
     static var previews: some View {
         ScrollView {
-            RecipeList(recipes: RecipeModel.recipes)
+            RecipeList(recipes: RecipeModel.recipes, category: .dinner)
         }
         
     }
