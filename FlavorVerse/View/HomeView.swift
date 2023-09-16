a6f53330812c252d2f72c1fc6177a7265d77b358
Lunick Francois <LunickFrancois22@gmail.com>
2023-09-15 21:43:58 -0400

    Update HomeView.swift

--- FlavorVerse/View/HomeView.swift
+++ FlavorVerse/View/HomeView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var selectedOption: Category = .breakfast
+    
+//    @EnvironmentObject var viewModel: RegistrationViewModel
+    
     var body: some View {
-        NavigationView {
-            ScrollView {
-                RecipeList(recipes: RecipeModel.recipes)
+        
+        VStack {
+            HStack {
+                Text("Welcome to FlavorVerse")
+                    .font(.title3)
+                    .multilineTextAlignment(.center)
+                    .bold()
+                    .foregroundColor(.black)
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "magnifyingglass")
+                        .font(.title2)
+                }
+
             }
+            // Menu Options list
+            MenuBarOptionsView(selectedOption: $selectedOption)
+                .padding([.top, .horizontal])
+            NavigationView {
+                ScrollView {
+                    RecipeList(recipes: RecipeModel.recipes)
+                }
+            }
+                
         }
         
     }
