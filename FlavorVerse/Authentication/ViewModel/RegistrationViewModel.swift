6bd5fa178ce25216eeadaa3b6626883781d55c14
Lunick Francois <LunickFrancois22@gmail.com>
2023-09-28 01:49:37 -0400

    Enhance registration error handling in RegistrationViewModel

--- FlavorVerse/Authentication/ViewModel/RegistrationViewModel.swift
+++ FlavorVerse/Authentication/ViewModel/RegistrationViewModel.swift
@@ -18,12 +18,52 @@
 
 import Foundation
 
+enum RegistrationError: Error {
+    case networkError
+    // Add more error cases as needed
+}
+
+
 class RegistrationViewModel: ObservableObject {
     @Published var email = ""
     @Published var password = ""
     @Published var username = ""
+    @Published var isRegistered = false
     
+    // Property to hold error messages
+    @Published var errorMessage = ""
+    
+    /// Attempts new user registration with the provided username, email, and password.
+    ///
+    /// - Throws: An error if the login attempt fails.
+    /// - Returns: A boolean indicating whether the login was successful.
     func createUser() async throws {
-        try await AuthService.shared.createUser(usingEmail: email, password: password, username: username)
+        // Reset error message
+        DispatchQueue.main.async {
+            self.errorMessage = ""
+        }
+        
+        // Check if email and password are not empty
+        guard !username.isEmpty, !email.isEmpty, !password.isEmpty else {
+            DispatchQueue.main.async {
+                self.errorMessage = "Please enter both email and password."
+            }
+            return
+        }
+        do {
+            try await AuthService.shared.createUser(usingEmail: email, password: password, username: username)
+            
+            isRegistered = true
+        } catch {
+            switch error {
+            case RegistrationError.networkError:
+                errorMessage = "Network error. Please check your internet connection and try again."
+            default:
+                errorMessage = "Failed to log in. Please try again later."
+            }
+            
+            isRegistered = false
+        }
+        
     }
 }
