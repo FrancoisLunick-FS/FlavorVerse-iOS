4aae939d4e3f47aa0e7ce65ab0294aacf0886374
Lunick Francois <LunickFrancois22@gmail.com>
2023-09-28 22:23:06 -0400

    Update LoginViewModel to check for user existence during login.

--- FlavorVerse/Authentication/ViewModel/LoginViewModel.swift
+++ FlavorVerse/Authentication/ViewModel/LoginViewModel.swift
@@ -17,6 +17,7 @@
 //
 
 import Foundation
+import FirebaseAuth
 
 // Enum to represent various login-related errors
 enum LoginError: Error {
@@ -46,42 +47,81 @@ class LoginViewModel: ObservableObject {
     /// - Throws: An error if the login attempt fails.
     /// - Returns: A boolean indicating whether the login was successful.
     func loginUser() async throws {
-        do {
-            // Reset error message
+        // Reset error message
+        DispatchQueue.main.async {
+            self.errorMessage = ""
+        }
+        
+        // Check if email and password are not empty
+        guard !email.isEmpty, !password.isEmpty else {
             DispatchQueue.main.async {
-                self.errorMessage = ""
+                self.errorMessage = "Please enter both email and password."
             }
-            
-            // Check if email and password are not empty
-            guard !email.isEmpty, !password.isEmpty else {
-                DispatchQueue.main.async {
-                    self.errorMessage = "Please enter both email and password."
-                }
-                return
+            return
+        }
+        guard isValidEmail(email) else {
+            DispatchQueue.main.async {
+                self.errorMessage = "Please enter a valid email."
             }
+            return
+        }
+
+        guard isValidPassword(password) else {
+            DispatchQueue.main.async {
+                self.errorMessage = "Please enter a valid password (minimum 8 characters)."
+            }
+            return
+        }
+        do {
             
             try await AuthService.shared.login(withemail: email, password: password)
-            // If the login succeeds, set isAuthenticated to true
-            DispatchQueue.main.async {
-                self.isAuthenticated = true
+            
+            if AuthService.shared.isUserExists(withEmail: email) {
+                // If the login succeeds, set isAuthenticated to true
+                DispatchQueue.main.async {
+                    self.isAuthenticated = true
+                }
             }
-        } catch {
+        } catch let error as NSError {
             // Handle login error here
-            switch error {
-            case LoginError.wrongPassword:
-                // Set an error message for wrong password
-                errorMessage = "Invalid email or password. Please check your credentials and try again."
-            case LoginError.networkError:
-                // Set an error message for network error
-                errorMessage = "Network error. Please check your internet connection and try again."
-            default:
-                // Set a generic error message for other login failures
-                errorMessage = "Failed to log in. Please try again later."
-            }
-            // Set isAuthenticated to false on login failure
-            DispatchQueue.main.async {
-                self.isAuthenticated = false
+            if let errorCode = error as NSError? {
+                switch errorCode.code {
+                case AuthErrorCode.invalidUserToken.rawValue:
+                    errorMessage = "User not found. Please check your email and try again."
+                case AuthErrorCode.invalidCredential.rawValue:
+                    errorMessage = "Wrong password. Please check your password and try again."
+                case AuthErrorCode.missingEmail.rawValue:
+                    errorMessage = "Email is missing. Please enter your email and try again."
+                case AuthErrorCode.userNotFound.rawValue:
+                    errorMessage = "User not found. Please Sign up or try again"
+                case AuthErrorCode.accountExistsWithDifferentCredential.rawValue:
+                    errorMessage = "Account exist with a different password. Please enter the correct password and try again"
+                case AuthErrorCode.invalidEmail.rawValue:
+                    errorMessage = "Email invalid. Please enter your correct email and try again."
+                case AuthErrorCode.networkError.rawValue:
+                    errorMessage = "Network error. Please check your internet connection and try again."
+                default:
+                    errorMessage = "Failed to log in. Please try again later."
+                }
+                // Set isAuthenticated to false on login failure
+                DispatchQueue.main.async {
+                    self.isAuthenticated = false
+                }
             }
         }
+    }
+    
+    // Email validation function
+    func isValidEmail(_ email: String) -> Bool {
+        // Check for a valid email format
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
+
+    // Password validation function
+    func isValidPassword(_ password: String) -> Bool {
+        // Check password criteria (e.g., minimum length)
+        return password.count >= 8
     }
 }
